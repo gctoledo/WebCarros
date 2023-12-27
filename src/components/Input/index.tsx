e81f5aea245e97ce6eb9d635f0fe5a0ce66c068e
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-12-27 20:23:19 -0300

    feat: add Login page and Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,33 @@
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+import { RegisterOptions, UseFormRegister } from "react-hook-form";
+import { FormData } from "../../pages/Login";
 
-const Input = (props: InputProps) => {
+type InputProps = {
+  placeholder: string;
+  type: string;
+  name: "email" | "password";
+  register: UseFormRegister<FormData>;
+  error?: string;
+  rules?: RegisterOptions;
+};
+
+const Input = ({
+  name,
+  type,
+  placeholder,
+  register,
+  error,
+  rules,
+}: InputProps) => {
   return (
     <div>
-      <input className="w-full border-2 rounded-md h-11 px-2" {...props} />
+      <input
+        className="w-full border-2 rounded-md h-11 px-2"
+        placeholder={placeholder}
+        type={type}
+        id={name}
+        {...register(name, rules)}
+      />
+      {error && <p className="my-1 text-red-500">{error}</p>}
     </div>
   );
 };
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,70 @@
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/logo.svg";
+import Container from "../../components/Container";
+import Input from "../../components/Input";
+
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Insira um e-mail válido")
+    .nonempty("O campo e-mail é obrigatório"),
+  password: z.string().nonempty("O campo de senha é obrigatório").min(3),
+});
+
+export type FormData = z.infer<typeof schema>;
+
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
+
+  const formSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to={"/"} className="mb-6 max-w-sm w-full">
+          <img src={logoImg} alt="Logo do site" className="max-w-sm w-full" />
+        </Link>
+
+        <form
+          className="bg-white max-w-xl w-full rounded-lg"
+          onSubmit={handleSubmit(formSubmit)}
+        >
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite o seu e-mail"
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite a sua senha"
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button type="submit">Acessar</button>
+        </form>
+      </div>
+    </Container>
   );
 }
 

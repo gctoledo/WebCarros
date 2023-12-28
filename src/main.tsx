cea224be94e5372015457ce01041caf18fc39eee
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-12-28 14:46:53 -0300

    feat: add provider to auth context with context api

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,12 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./App";
 
 import "./index.css";
+import AuthProvider from "./contexts/authContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );

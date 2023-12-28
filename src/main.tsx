c26042f24a8c914bdd1dc33d1143ea807e12a32b
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-12-28 15:09:21 -0300

    fix: AuthContext import

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./App";
 
 import "./index.css";
-import AuthProvider from "./contexts/authContext";
+import AuthProvider from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

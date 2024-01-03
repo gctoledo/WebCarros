c644f2afbc878c0bd3277923b80e4cce715182e9
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-01-03 16:30:15 -0300

    fix: add error handling for accessing non-existent ad pages

--- src/pages/CarDetails/index.tsx
+++ src/pages/CarDetails/index.tsx
@@ -5,7 +5,7 @@ import Container from "../../components/Container";
 
 import { FaWhatsapp } from "react-icons/fa";
 
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { CarProps } from "../Home";
 
@@ -29,6 +29,8 @@ function CarDetails() {
 
   const { id } = useParams();
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     async function loadCar() {
       if (!id) return;
@@ -36,6 +38,10 @@ function CarDetails() {
       const docRef = doc(db, "cars", id);
 
       getDoc(docRef).then((snapshot) => {
+        if (snapshot.data() === undefined) {
+          navigate("/");
+        }
+
         setCar({
           id: snapshot.id,
           name: snapshot.data()?.name,
@@ -77,21 +83,23 @@ function CarDetails() {
 
   return (
     <Container>
-      <Swiper
-        slidesPerView={sliderPerView}
-        pagination={{ clickable: true }}
-        navigation
-      >
-        {car?.images.map((image) => (
-          <SwiperSlide key={image.name}>
-            <img
-              src={image.url}
-              className="w-full h-96 object-cover"
-              alt={image.name}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {car && (
+        <Swiper
+          slidesPerView={sliderPerView}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {car?.images.map((image) => (
+            <SwiperSlide key={image.name}>
+              <img
+                src={image.url}
+                className="w-full h-96 object-cover"
+                alt={image.name}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
 
       {car && (
         <main className="w-full bg-white rounded-lg p-6 my-4">
@@ -130,7 +138,8 @@ function CarDetails() {
 
           <a
             className="bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-xl rounded-lg font-medium cursor-pointer"
-            href="$"
+            href={`http://api.whatsapp.com/send?phone=${car?.whatsapp}&text=Gostaria de falar sobre o anuncio do carro ${car?.name}.`}
+            target="_blank"
           >
             Conversar com vendedor <FaWhatsapp size={26} color="#fff" />
           </a>
